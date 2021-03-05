d455acaf9aecfd338dcd02043737ca25d0b0ebce
denamyte <denamyte@gmail.com>
2021-03-05 20:10:29 +0200

    2021-03-05: Book 'Introducing Go' Chapter 8: in progress 3

---
+++ books/introducing-go/chapter08/samples/15_parse_command_line_args/15_parse_command_line_args.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	// Define flags
+	maxp := flag.Int("max", 6, "the max value")
+	// Parse
+	flag.Parse()
+	// Generate a number between 0 and max
+	rand.Seed(time.Now().Unix())
+	fmt.Println(rand.Intn(*maxp))
+}
